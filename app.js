6bb84387c0010e485a1952507a04384bbb48b7f1
Cameron <cameron.cw@outlook.com>
2023-09-27 13:37:02 +1000

    Practise with JavaScript

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-let cash = 20
+let cash = 50
 let price = 40
-let difference = cash - price
+let isStoreOpen = false
 
-if (cash > price) {
-    console.log (`you paid extra - here's ${difference} dollars change`)
-}
-else if (cash === price) {
-    console.log("you paid the exact amount, have a nice day!")
-}
-else {
-    console.log(`not enough money - you still owe ${difference * -1} dollars`)
+if (cash >= price && isStoreOpen === false) {
+    console.log('print the receipt')
 }
