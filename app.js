21ae41360ebdec6d2deaa292462b3c8c6efa61ba
Cameron <cameron.cw@outlook.com>
2023-09-27 23:47:53 +1000

    More JavaScript Practise

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let cash = 50
 let price = 40
-let isStoreOpen = false
+let isStoreOpen = true
 
-if (cash >= price && isStoreOpen === false) {
-    console.log('print the receipt')
-}
+let str = cash >= price && isStoreOpen ? 'give receipt' : 'do not give receipt'
+console.log(str)
