01220bab135f627457534156413a7891621e2c13
Cameron <cameron.cw@outlook.com>
2025-06-30 21:10:40 +0100

    Using Practise Functions

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-for (let i = 1; i <= 20; ++i) {
-if (i % 3 === 0 && i % 5 === 0) {
-  console.log(`${i} -> Frontend Simplified`);
-} else if (i % 3 === 0) {
-  console.log(`${i} -> Frontend`);
-} else if (i % 5 === 0) {
-  console.log(`${i} -> Simplified`);
-} else {
-  console.log(`${i} -> ${i}`);
+const convertCelsiusToFahrenheit = (celsius) => {
+return celsius * 1.8 + 32
 }
-}
+
+console.log(convertCelsiusToFahrenheit(0));
