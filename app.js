57731619648a22cee26e969fc255ec5b985b9713
Cameron <cameron.cw@outlook.com>
2025-07-08 13:28:40 +0100

    Login function for users

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let users = [
   {
     username: "Cameron",
     email: "Cameron@frontendsimplified.com",
+    password: 'test123',
     subscribtionStatus: "VIP",
     discordId: "CamboWarbo",
     lessonsCompleted: [0, 1],
@@ -9,10 +10,27 @@ let users = [
   {
     username: "Mitri",
     email: "Mitri@frontendsimplified.com",
+    password: 'mitri123',
     subscribtionStatus: "VIP",
     discordId: "Mitri#0001",
     lessonsCompleted: [0, 1, 2, 3],
   },
+  {
+    username: "Zen",
+    email: "Zen@frontendsimplified.com",
+    password: 'Zen123',
+    subscribtionStatus: "VIP",
+    discordId: "Zen#0001",
+    lessonsCompleted: [0, 1, 2, 3],
+  },
 ];
 
-console.log(users[0].lessonsCompleted.map(elem => elem * 2))
+function login(email, password) {
+  for (let i = 0; i < users.length; ++i) {
+    if (users[i].email === email) {
+    console.log(users[i]);
+    }
+  }
+}
+
+login('Cameron@frontendsimplified.com', 'test123');
