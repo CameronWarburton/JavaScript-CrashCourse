ac0f9c3a1025cba6425c993819ae41ef9c3de780
Cameron <cameron.cw@outlook.com>
2025-07-18 14:31:02 +0100

    Going back over API's and Promises

--- app.js
+++ app.js
@@ -1,36 +1,20 @@
-let users = [
-  {
-    username: "Cameron",
-    email: "Cameron@frontendsimplified.com",
-    password: 'test123',
-    subscribtionStatus: "VIP",
-    discordId: "CamboWarbo",
-    lessonsCompleted: [0, 1],
-  },
-  {
-    username: "Mitri",
-    email: "Mitri@frontendsimplified.com",
-    password: 'mitri123',
-    subscribtionStatus: "VIP",
-    discordId: "Mitri#0001",
-    lessonsCompleted: [0, 1, 2, 3],
-  },
-  {
-    username: "Zen",
-    email: "Zen@frontendsimplified.com",
-    password: 'Zen123',
-    subscribtionStatus: "VIP",
-    discordId: "Zen#0001",
-    lessonsCompleted: [0, 1, 2, 3],
-  },
-];
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+const emailRef = document.querySelector(".email");
 
-function login(email, password) {
-  for (let i = 0; i < users.length; ++i) {
-    if (users[i].email === email) {
-    console.log(users[i]);
-    }
-  }
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email;
+//   });
+
+async function main() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const data = await response.json();
+  console.log(data);
+  emailRef.innerHTML = data.email;
 }
 
-login('Cameron@frontendsimplified.com', 'test123');
+main();
