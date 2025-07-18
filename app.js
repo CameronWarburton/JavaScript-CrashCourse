ed2329f21a0eae3f6a6a8ad6bbf783555948caed
Cameron <cameron.cw@outlook.com>
2025-07-18 15:01:58 +0100

    Creating Promises and unlocking promises

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-const emailRef = document.querySelector(".email");
+const statusRef = document.querySelector(".status");
+const videoRef = document.querySelector(".video");
 
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     emailRef.innerHTML = data.email;
-//   });
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(undefined);
+    }, 2000);
+  });
+}
+
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("show video");
+    }
+    else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
+    }
+  });
+}
 
 async function main() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
-  const data = await response.json();
-  console.log(data);
-  emailRef.innerHTML = data.email;
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
+  try {
+  console.log(await getVideo(status));
+  }
+  catch (e) {
+    console.log(e);
+    videoRef.innerHTML = e;
+  }
 }
 
 main();
